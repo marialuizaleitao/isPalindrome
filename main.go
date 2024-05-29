7356553161544996705abbb696bdf9815d960368
Maria Leitão <malufln.99@gmail.com>
2024-05-29 14:48:21 -0300

    Implement invertText function for reversing strings

--- main.go
+++ main.go
@@ -1,4 +1,15 @@
 package main
 
+import "fmt"
+
 func main() {
+	fmt.Println(invertText("hello world"))
+}
+
+func invertText(text string) string {
+	var invertedText string
+	for i := len(text) - 1; i >= 0; i-- {
+		invertedText += string(text[i])
+	}
+	return invertedText
 }
